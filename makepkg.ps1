0e26e0d49f1cc17eb3f39c98df4e8701e06115f1
LCA-EET <LCAMod@danielvalle.net>
2026-06-29 08:44:59 -0400

    update

--- makepkg.ps1
+++ makepkg.ps1
@@ -1,15 +1,23 @@
+$weiduApps		= @("weidu.exe", "weidu_linux")
+$weiduExts		= @(".exe", "")
+$weiduArchives 	= @("_win", "_linux")
+
 $currentDir = (Get-Location).Path
 $basePath = "SPC"
 $tp2Name = "SPC"
 $modPath = $basePath + "/" + $tp2Name 
 $archive = $basePath + ".zip"
-$exePath = "setup-" + $tp2Name + ".exe"
+$exePath = "setup-" + $tp2Name
 $folders= @(
 'baf',
 'bmp',
 'spl',
 'tra'
 )
+
+foreach($weiduArchive in $weiduArchives){
+	Remove-Item -LiteralPath ($basePath + $weiduArchive + ".zip") -Force
+}
 
 Remove-Item -LiteralPath $archive -Force
 Remove-Item -LiteralPath $basePath -Force -Recurse
@@ -21,28 +29,41 @@ foreach($folder in $folders){
 }
 
 Copy-Item -Path ($tp2Name + ".tp2") -Destination $modPath 
-Copy-Item -Path "weidu.exe" -Destination ($basePath + "/" + $exePath)
-
 Copy-Item -Path "Discord Server.url" -Destination $modPath
 
 Get-Date -Format "yyyy-MM-dd HH:mm K" > pkgdate.txt
 
 Copy-Item -Path pkgdate.txt -Destination $modPath
-$Source = "./" + $basePath + "/*"
-$Target = "./" + $archive
 
-$7zipPath = "F:/Program Files/7-Zip/7z.exe"
+for ($i = 0; $i -lt $weiduApps.Length; $i++) {
+	if($i -gt 0){
+		Write-Output "Deleting " ($basePath + "/" + $exePath + $weiduExts[$i-1])
+		Remove-Item -LiteralPath ($basePath + "/" + $exePath + $weiduExts[$i-1])
+	}
+	Copy-Item -Path $weiduApps[$i] -Destination ($basePath + "/" + $exePath + $weiduExts[$i])
+	
+	$archive = $basePath + $weiduArchives[$i] + ".zip"
+	$Source = "./" + $basePath + "/*"
+	$Target = "./" + $archive
 
-if(Test-Path -Path $7zipPath){
-	Set-Alias Start-SevenZip $7zipPath
-	Start-SevenZip a -mx=9 $Target $Source
-}
-else{
-	#Linux
-	7z a -mx=9 $Target $Source
+	$7zipPath = "F:/Program Files/7-Zip/7z.exe"
+
+	if(Test-Path -Path $7zipPath){
+		Set-Alias Start-SevenZip $7zipPath
+		Start-SevenZip a -mx=9 $Target $Source
+	}
+	else{
+		#Linux
+		7z a -mx=9 $Target $Source
+	}
+
+	Remove-Item -LiteralPath $basePath -Force -Recurse
+	Get-FileHash $archive -Algorithm SHA256 > SHA256.txt
+
+	Copy-Item -Path $archive -Destination ("\\nas.home.lan\smbuser\Home\Installers\" + $archive)
+	
 }
 
 Remove-Item -LiteralPath $basePath -Force -Recurse
-Get-FileHash $archive -Algorithm SHA256 > SHA256.txt
 
-Copy-Item -Path $archive -Destination ("\\nas.home.lan\smbuser\Home\Installers\" + $archive)
+
